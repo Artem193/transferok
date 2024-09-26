aad9010db0142a41a810e1f289c9b7a519e35b7a
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2024-09-26 15:56:24 +0300

    fixed buttons phone

--- src/components/blocks/footer/Footer.jsx
+++ src/components/blocks/footer/Footer.jsx
@@ -4,23 +4,28 @@ import './footer.scss';
 
 export const Footer = () => {
   return (
-    <div className="footer">
-      <h1 className="footer__title">Контакти</h1>
-      <p className="footer__text">
-        Номер телефону:&nbsp;
-        <a href="tel:+380509573836" className="footer__link">
-          +38 0 (50) 957 38 36
+    <div>
+      <footer className="footer">
+        <h1 className="footer__title">Контакти</h1>
+        <p className="footer__text">
+          Номер телефону:&nbsp;
+          <a href="tel:+380509573836" className="footer__link">
+            +38 0 (50) 957 38 36
+          </a>
+        </p>
+        <p className="footer__text">
+          Email:&nbsp;
+          <a href="mailto:Tylpan7788@gmail.com" className="footer__link">
+            Tylpan7788@gmail.com
+          </a>
+        </p>
+        <a href="#">
+          <div className="footer__logo"></div>
         </a>
+      </footer>
+      <p className="footer__copyright">
+        © 2024 Transfer-OK. Все права защищены.
       </p>
-      <p className="footer__text">
-        Email:&nbsp;
-        <a href="mailto:Tylpan7788@gmail.com" className="footer__link">
-          Tylpan7788@gmail.com
-        </a>
-      </p>
-      <a href="#">
-        <div className="footer__logo"></div>
-      </a>
     </div>
   )
 }
